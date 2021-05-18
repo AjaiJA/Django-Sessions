d326c50b7c35d873d78db95a2272e8e023ac3ee6
Ajai J A <57520660+AjaiJA@users.noreply.github.com>
2021-05-19 02:14:03 +0530

    Updated

--- Djan/settings.py
+++ Djan/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8ga7a1mqel@tt-xn%j4bp#&c%$xomdw1(utnir2zpjo*ci!$v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
